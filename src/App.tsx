a07bac1f53556260e6701cb2d05bb485a6e8e185
Victor Mann <victorcmann@gmail.com>
2022-06-16 16:01:39 -0300

    interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png';
+import RestartIcon from './svgs/restart.svg';
+
+import { InfoItem } from './components/InfoItem';
+import { Button } from './components/Button';
+import { GridItemType } from './types/GridItemType';
+import { GridItem } from './components/GridItem';
+
+import { items } from './data/items';
+
 
 function App() {
+  const [palying, setPlaying] = useState<boolean>(false);
+  const [timeElapsed, setTimeELapsed] = useState<number>(0);
+  const [moveCount, setMoveCount] = useState<number>(0);
+  const [shownCount, setShownCount] = useState<number>(0);
+  const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+
+  useEffect(() => resetAndCreateGrid(), []);
+
+  const resetAndCreateGrid = () => {
+    // passo 1 - resetar o jogo
+    setTimeELapsed(0);
+    setMoveCount(0);
+    setShownCount(0);
+
+    // passo 2 - criar o grid
+    // 2.1 criar grid vazio
+    let tmpGrid: GridItemType[] = [];
+    for (let i = 0; i < (items.length * 2); i ++) {
+      tmpGrid.push({
+        item: null,
+        shown: false,
+        permanentShown: false
+      });
+    }
+    // 2.2 preencher grid
+    for (let w = 0; w < 2; w ++) {
+      for (let x = 0; x < items.length; x ++) {
+        let pos: number = -1;
+        while (pos < 0 || tmpGrid[pos].item !== null) {
+          pos = Math.floor(Math.random() * (items.length * 2));  
+        }
+        tmpGrid[pos].item = x;
+      }
+    }
+
+    // 2.3 jogar no state
+    setGridItems(tmpGrid);
+
+    // passo 3 - começar 
+    setPlaying(true);
+  };
+
+  const handleItemClick = (index: number) => {
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} width="200" alt="" />
+        </C.LogoLink>
+
+        <C.InfoArea>
+          <InfoItem label="Tempo" value="00:00" />
+          <InfoItem label="Movimentos" value="0" />
+        </C.InfoArea>
+
+        <Button label="Reiniciar" icon={RestartIcon} onClick={resetAndCreateGrid} />
+
+      </C.Info>
+
+
+      <C.GridArea>
+        <C.Grid>
+          {gridItems.map((item, index) =>
+            <GridItem 
+              key={index} 
+              item={item}
+              onClick={() => handleItemClick(index)} />
+          )}
+        </C.Grid>
+      </C.GridArea>
+
+
+    </C.Container>
   );
 }
 
